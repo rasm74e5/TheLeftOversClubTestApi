74ff76387c9b4fd839600bae2ae1168bac81fcbd
Rasmus Bach Jørgensen <rasm74e5@edu.eal.dk>
2019-11-21 13:21:39 +0100

    Add project files.

---
+++ TheLeftOversClubTestApi/Controllers/ValuesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheLeftOversClubTestApi.Data;
+using TheLeftOversClubTestApi.Models;
+using static TheLeftOversClubTestApi.Data.ProductContext;
+
+namespace TheLeftOversClubTestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ProductContext _context;
+        public ValuesController(ProductContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.products.ToListAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(long id)
+        {
+            var product = await _context.products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _context.products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductID }, product);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(long id, Product product)
+        {
+            if (id != product.ProductID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(long id)
+        {
+            var product = await _context.products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
